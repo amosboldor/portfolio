d430a565a24ef34c35144725b61c7624789cf106
Amos Boldor <amosboldor@gmail.com>
2016-10-28 09:57:01 -0700

    replace code and use handlebars template

--- scripts/myProjects.js
+++ scripts/myProjects.js
@@ -1,20 +1,20 @@
 var myProjects = [
   {
-    snapShotUrl: '/pics/bus_mall.jpg',
+    snapShotUrl: 'pics/bus_mall.jpg',
     name: 'Bus Mall',
     timeTook: '1 week',
     githubUrl: 'https://github.com/amosboldor/bus-mall',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
   {
-    snapShotUrl: '/pics/cookie_stand.png',
+    snapShotUrl: 'pics/cookie_stand.png',
     name: 'Cookie Stand',
     timeTook: '1 week',
     githubUrl: 'https://github.com/amosboldor/cookie-stand',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
   {
-    snapShotUrl: '/pics/century_cycle.png',
+    snapShotUrl: 'pics/century_cycle.png',
     name: 'Century Cycle Tracker',
     timeTook: '1 week',
     githubUrl: 'https://github.com/Chaimaebettah/finalProject201',
