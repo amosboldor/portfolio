f38f6673095625e0e8f7364b0f9f357aa141af34
Amos Boldor <amosboldor@gmail.com>
2016-11-04 12:36:27 -0700

    wrap code in IFFE's

--- scripts/views/projectView.js
+++ scripts/views/projectView.js
@@ -1,19 +1,23 @@
-var projectView = {};
+(function (module) {
+  var projectView = {};
 
-projectView.handleMainNav = function () {
-  $('.main-nav').on('click', '.tab', function() {
-    $('.tab-content').hide();
-    $('main section[id=\"' + $(this)[0].attributes[1].value + '\"]').fadeIn();
-    $('main section[id=\"project-' + $(this)[0].attributes[1].value + '\"]').fadeIn();
-  });
-};
+  projectView.handleMainNav = function () {
+    $('.main-nav').on('click', '.tab', function() {
+      $('.tab-content').hide();
+      $('main section[id=\"' + $(this)[0].attributes[1].value + '\"]').fadeIn();
+      $('main section[id=\"project-' + $(this)[0].attributes[1].value + '\"]').fadeIn();
+    });
+  };
 
-projectView.renderIndexPage = function() {
-  Project.projects.forEach(function(a){
-    $('#projects').append(a.toHtml());
-  });
-  projectView.handleMainNav();
-  $('footer').html('<p>' + 'Number of words in every description: ' + Project.numWordsAll() + '</p>');
-};
+  projectView.renderIndexPage = function() {
+    Project.projects.forEach(function(a){
+      $('#projects').append(a.toHtml());
+    });
+    projectView.handleMainNav();
+    $('footer').html('<p>' + 'Number of words in every description: ' + Project.numWordsAll() + '</p>');
+  };
 
-Project.fetchAll();
+  Project.fetchAll();
+
+  module.projectView = projectView;
+})(window);
